a6931211be9a8e1e419049dedf152e8b3bd8f105
yousufAlansary <yousuf.alansari@gmail.com>
2023-02-06 15:55:02 +0300

    Alpha

--- src/Sorter.java
+++ src/Sorter.java
@@ -3,10 +3,28 @@ import java.util.Collections;
 
 public class Sorter {
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        for (int i = 0; i < nums.size(); i++) {
-            nums.add(i);
+        double[] nums = new double[30];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.random() * 30;
         }
-        Collections.shuffle(nums);
+        System.out.println(nums);
+        doSelectionSort(nums);
+        System.out.println(nums);
+    }
+    public static double[] doSelectionSort(double[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int pos = i;
+            // find position of smallest num between (i + 1)th element and last element
+            for (int j = i + 1; j <= arr.length; j++) {
+                if (arr[j] < arr[pos])
+                    pos = j;
+    
+                // Swap min (smallest num) to current position on array
+                double min = arr[pos];
+                arr[pos] = arr[i];
+                arr[i] = min;
+            }
+        }
+        return arr;
     }
 }
